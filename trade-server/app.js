095dfad33d1726a47018670ba91a168cba35f568
Gary Linscott <glinscott@gmail.com>
2012-01-02 23:38:22 -0500

    Add basic express server

--- trade-server/app.js
+++ trade-server/app.js
@@ -1,4 +1,27 @@
 // External dependencies
 var async = require('async');
 var bigint = require('bigint');
+var express = require('express');
 
+var app = express.createServer();
+
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+});
+
+app.configure('development', function() {
+ 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function() {
+ 	app.use(express.errorHandler()); 
+});
+
+app.configure('test', function() {
+ 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 	
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
