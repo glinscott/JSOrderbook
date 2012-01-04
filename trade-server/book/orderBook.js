cdda76da4266bbb859742976b6a8bac717ba99d9
Gary Linscott <glinscott@gmail.com>
2012-01-03 22:28:03 -0500

    Core concepts for the order book

---
+++ trade-server/book/orderBook.js
@@ -0,0 +1,46 @@
+function OrderBook(){};
+
+//////////////////////////////////////
+// Core
+//////////////////////////////////////
+
+// Limit Order
+// Places an order at a given price.  Can be a bid or ask.
+OrderBook.prototype.limit = function(order) {
+	
+};
+
+// Cancels the given order, removing it from the book.
+OrderBook.prototype.cancel = function(orderId) {
+	
+};
+
+// Returns the best bid price
+OrderBook.prototype.bestBid = function() {
+	
+};
+
+// Returns the best ask price
+OrderBook.prototype.bestAsk = function() {
+	
+};
+
+//////////////////////////////////////
+// Advanced Order Types
+//////////////////////////////////////
+
+// Market Order - executes immediately at the best price possible.
+// Returns the id of the executed order.
+OrderBook.prototype.market = function(order) {
+};
+
+// Replaces the given orderId, with newOrder
+OrderBook.prototype.replace = function(orderId, newOrder) {
+	
+};
+
+// Adds an order that is guaranteed to add liquidity to the market.  Prices
+// the order at the best price + offset
+OrderBook.prototype.post = function(order, offset) {
+	
+};
