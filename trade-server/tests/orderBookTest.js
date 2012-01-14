2d5937a834bd62ad961613017c646cfb9449193b
Gary Linscott <glinscott@gmail.com>
2012-01-13 19:19:24 -0500

    Slight test refactoring, easier to verify book orders

--- trade-server/tests/orderBookTest.js
+++ trade-server/tests/orderBookTest.js
@@ -8,29 +8,22 @@ function createOrder(type, price, numShares) {
 	return new Order.Order(orderId++, type, price, numShares, 0);
 }
 
+function placeOrder(book, order, bestBid, bestAsk, totalBidVol, totalAskVol) {
+	var id = book.limit(order);
+	
+	assert.equal(book.bestBid(), bestBid);
+	assert.equal(book.bestAsk(), bestAsk);
+	
+	return {id: id, order: order};
+}
+
 function testBasics() {
 	var book = new OrderBook.OrderBook();
 	
-	var order1 = createOrder(Order.OrderTypes.Bid, 15, 10);
-	var id1 = book.limit(order1);
-	
-	var order2 = createOrder(Order.OrderTypes.Ask, 12, 5);
-	var id2 = book.limit(order2);
-
-	assert.equal(book.bestBid(), 15);
-	assert.equal(book.bestAsk(), Price.NoAsk);
-	
-	var order3 = createOrder(Order.OrderTypes.Ask, 16, 10);
-	var id3 = book.limit(order3);
-	
-	assert.equal(book.bestBid(), 15);
-	assert.equal(book.bestAsk(), 16);
-	
-	var order4 = createOrder(Order.OrderTypes.Ask, 14, 6);
-	var id4 = book.limit(order4);
-	
-	assert.equal(book.bestBid(), Price.NoBid);
-	assert.equal(book.bestAsk(), 14);
+	placeOrder(book, createOrder(Order.OrderTypes.Bid, 15, 10), 15, Price.NoAsk, 10, 0);
+	placeOrder(book, createOrder(Order.OrderTypes.Ask, 12, 5), 15, Price.NoAsk, 5, 0);
+	placeOrder(book, createOrder(Order.OrderTypes.Ask, 16, 10), 15, 16, 5, 10);
+	placeOrder(book, createOrder(Order.OrderTypes.Ask, 14, 6), Price.NoBid, 14, 0, 14)
 }
 
 testBasics();
